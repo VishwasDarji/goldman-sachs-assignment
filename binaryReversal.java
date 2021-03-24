9062e43df09264beaf4d9c26f2253c2cfb239c9b
Vishwas Darji <37459438+VishwasDarji@users.noreply.github.com>
2021-03-24 16:15:23 +0530

    assignment commits

---
+++ binaryReversal.java
@@ -0,0 +1,36 @@
+
+public class binaryReversal {
+	
+	// function to reverse bits of a number
+    public static int reverseBinary(int n)
+    {
+        int rev = 0;
+  
+        // traversing bits of 'n' 
+        // from the right
+        while (n > 0) 
+        {
+            // bitwise left shift 
+            // 'rev' by 1
+            rev <<= 1;
+  
+            // if current bit is '1'
+            if ((int)(n & 1) == 1)
+                rev ^= 1;
+  
+            // bitwise right shift 
+            //'n' by 1
+            n >>= 1;
+        }
+        // required number
+        return rev;
+    }
+
+	public static void main(String[] args) {
+		
+		int n = 47;
+        System.out.println(reverseBinary(n));
+
+	}
+
+}
